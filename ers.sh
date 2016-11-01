ac097f72356a5577be58812a6dc677dde521f388
n0rbertt <xxrelativityxx@gmail.com>
2016-11-01 12:30:43 -0400

    Update ers.sh

--- ers.sh
+++ ers.sh
@@ -5,3 +5,8 @@ echo "Which user would you like to have privileges?"
 read user
 sed -i "99 i  $user ALL=(ALL)       NOPASSWD: ALL" /etc/sudoers
 
+## I was just thinking it would be nice if I could have this done while I run my install script: 
+## Just add the following into your script:
+
+user=`last | awk '{print $1}' | head -n1'
+sed -i "99 i  $user ALL=(ALL)       NOPASSWD: ALL" /etc/sudoers
