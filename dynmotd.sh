f9c3e1fd923b248c9fa7739f48f49821908d5e09
n0rbertt <xxrelativityxx@gmail.com>
2017-05-24 12:27:34 -0400

    Update dynmotd.sh

--- dynmotd.sh
+++ dynmotd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yum install cowsay fortune-mod -y
+# yum install cowsay fortune-mod -y
 
 echo "PrintMotd no" >> /etc/ssh/sshd_config && echo "clear && /usr/local/bin/dynmotd" >> /etc/profile && touch /usr/local/bin/dynmotd && chmod 755 /usr/local/bin/dynmotd
 
