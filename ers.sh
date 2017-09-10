083fb43905c23e50f70ba1bd5d0437e5904d0e0b
n0rbertt <xxrelativityxx@gmail.com>
2017-09-10 05:46:36 -0400

    Update ers.sh

--- ers.sh
+++ ers.sh
@@ -6,7 +6,7 @@ read user
 sed -i "99 i  $user ALL=(ALL)       NOPASSWD: ALL" /etc/sudoers
 
 ## I was just thinking it would be nice if I could have this done while I run my install script: 
-## Just add the following into your script:
+## Just add the following into your script(without the '#':
 
-user=`last | awk '{print $1}' | head -n1'
-sed -i "99 i  $user ALL=(ALL)       NOPASSWD: ALL" /etc/sudoers
+#user=`last | awk '{print $1}' | head -n1'
+#sed -i "99 i  $user ALL=(ALL)       NOPASSWD: ALL" /etc/sudoers
